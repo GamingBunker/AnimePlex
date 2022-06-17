15b7c08c55633fef43efe11b35e6eee4fc2ad461
Mattia <mattia.ceschin@gmail.com>
2022-06-17 13:46:37 +0200

    init

---
+++ src/web/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import animeView from "../views/animeView.vue"
+import mangaView from "../views/mangaView.vue"
+import resourceView from "../views/resourceView.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    /*{
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }*/
+    {
+      path: "/anime",
+      name: "anime",
+      component: animeView
+    },
+    {
+      path: "/manga",
+      name: "manga",
+      component: mangaView
+    },
+    {
+      path: "/resource",
+      name: "resource",
+      component: resourceView
+    }
+  ]
+})
+
+export default router
---
+++ src/web/src/views/mangaView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <!-- Problem img tag src 403-->
+    <meta name="referrer" content="no-referrer" /> 
+    
+    <div>
+      <SearchComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchComponent from "../components/searchComponent.vue";
+  export default {
+    components:{ SearchComponent }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
