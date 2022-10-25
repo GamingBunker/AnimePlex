4f0d2305ca96eb1e585fe3b494fb77f759277286
Cesxhin <>
2022-10-25 20:41:16 +0200

    Create base nuxt refactory

---
+++ src/web_refactory/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
