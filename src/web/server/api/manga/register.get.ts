c373f0365868220918607811463c1e64b77661ca
Cesxhin <>
2022-12-11 20:48:32 +0100

    Update env

--- src/web/server/api/manga/register.get.ts
+++ src/web/server/api/manga/register.get.ts
@@ -1,7 +1,7 @@
 import {defineEventHandler, getQuery} from "h3";
 import axios from "axios";
 
-const API_BASE = process.env.API_BASE_URL;
+const API_BASE = process.env.API_BASE_URL_BASE;
 
 export default defineEventHandler(async (event) => {
     const {id} = getQuery(event)
